732461ca4205a3a67af40af28fb2d48b466a43dd
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2024-03-28 19:22:35 +0500

    Настроил в моделях внешние ключи + настроил нормальное отображение интерфейса(в стилях закомментил body: display flex)

--- main/models.py
+++ main/models.py
@@ -3,10 +3,19 @@ from django.db import models
 class Mark(models.Model):
     latitude = models.FloatField('Широта')
     longitude = models.FloatField('Долгота')
-    # user_id = models.ForeignKey(name='ID пользователя', to='Content', on_delete=models.CASCADE)
-    user_id = models.IntegerField('ID пользователя')
-    content_id = models.IntegerField('ID контента')
-    # content_id = models.ForeignKey(name='ID контента', to='Content', on_delete=models.CASCADE)
+
+    user_id = models.ForeignKey(
+        verbose_name='ID пользователя',
+        to='auth.user',
+        on_delete=models.CASCADE)
+    # user_id = models.IntegerField(verbose_name='ID пользователя')
+
+    # content_id = models.IntegerField('ID контента')
+    content_id = models.ForeignKey(
+        verbose_name='ID контента',
+        to='Content',
+        on_delete=models.CASCADE)
+
     objects = models.Manager()
 
     def __str__(self):
@@ -29,8 +38,11 @@ class Content(models.Model):
 
 class Photo(models.Model):
     photo = models.ImageField('Фото')
-    # content_id = models.ForeignKey(name='ID контента', to='Content', on_delete=models.CASCADE)
-    content_id = models.IntegerField('ID контента')
+    # content_id = models.IntegerField('ID контента')
+    content_id = models.ForeignKey(
+        verbose_name='ID контента',
+        to='Content',
+        on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Контент №{self.content_id}'
