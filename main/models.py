af14a151b9acd978a3c1c17c8fd2120005eac424
maksimsadkov <sy_sy_sy_sy@mail.ru>
2024-06-12 18:24:59 +0500

    Добавлены модели для достижений

--- main/models.py
+++ main/models.py
@@ -57,3 +57,48 @@ class Photo(models.Model):
     class Meta:
         verbose_name = 'Фото'  # Название таблицы в единственном числе
         verbose_name_plural = 'Фотографии'  # Название таблицы в множественном числе
+
+
+class Achievements(models.Model):
+    name = models.CharField('Название', max_length=50)
+    description = models.TextField('Описание')
+    points = models.IntegerField('Очки')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Levels(models.Model):
+    points_for_level = models.IntegerField('Кол-во очков уровня')
+
+    def __str__(self):
+        return f'Кол-во очков уровня: {self.points_for_level}'
+
+
+class UsersAchievements(models.Model):
+    user_id = models.ForeignKey(
+        verbose_name='ID пользователя',
+        to='auth.user',
+        on_delete=models.CASCADE)
+    achievement_id = models.ForeignKey(
+        verbose_name='ID достижения',
+        to='Achievements',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'Пользователь: {self.user_id}, Достижения: {self.achievement_id}'
+
+
+class UsersLevel(models.Model):
+    user_id = models.ForeignKey(
+        verbose_name='ID пользователя',
+        to='auth.user',
+        on_delete=models.CASCADE)
+    level = models.IntegerField('Уровень пользователя')
+    total_points = models.IntegerField('Очки пользователя')
+
+    def __str__(self):
+        return f'Пользователь: {self.user_id}, Текущий уровень: {self.level}'
